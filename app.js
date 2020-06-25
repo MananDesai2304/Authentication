ed3ba65b92e5e4a37f211e4d64f83d565060df1a
MananDesai2304 <manandesai575@gmial.com>
2020-06-26 00:49:07 +0530

    Use Hasing Password

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -15,7 +16,7 @@ const userSchema = new mongoose.Schema({
   email : String,
   password : String
 });
-userSchema.plugin(encrypt,{ secret:process.env.SECRET,encryptedFields:["password"] });
+//userSchema.plugin(encrypt,{ secret:process.env.SECRET,encryptedFields:["password"] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -35,7 +36,7 @@ app.get("/login",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -51,7 +52,7 @@ app.post("/login",function(req,res){
         if(err){
           console.log(err);
         }else{
-          if(foundUser.password === req.body.password){
+          if(foundUser.password === md5(req.body.password)){
               res.render("secrets");
           }else{
             res.send("Enter Correct Password");
