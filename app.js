cfbe544ed104b117de3bb6d1964827e71bbecbf6
MananDesai2304 <manandesai575@gmial.com>
2020-06-26 13:25:15 +0530

    Add Salting to password

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRound = 10;
 const app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+
 
 mongoose.connect("mongodb://localhost:27017/userDB",{ useNewUrlParser: true,useUnifiedTopology: true });
 const userSchema = new mongoose.Schema({
@@ -34,29 +38,58 @@ app.get("/login",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email : req.body.username,
-    password : md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
+    bcrypt.hash(req.body.password,saltRound,function(err,hash){
+        if(err){
+          console.log(err);
+        }else{
+          const newUser = new User({
+            email : req.body.username,
+            password : hash
+          });
+          newUser.save(function(err){
+            if(err){
+              console.log(err);
+            }else{
+              res.render("secrets");
+            }
+          });
+        }
+
+
+    });
+  // const newUser = new User({
+  //   email : req.body.username,
+  //   password : md5(req.body.password)
+  // });
+  // newUser.save(function(err){
+  //   if(err){
+  //     console.log(err);
+  //   }else{
+  //     res.render("secrets");
+  //   }
+  // });
 });
 
 app.post("/login",function(req,res){
+
+
     User.findOne({email:req.body.username},function(err,foundUser){
         if(err){
           console.log(err);
         }else{
-          if(foundUser.password === md5(req.body.password)){
-              res.render("secrets");
-          }else{
-            res.send("Enter Correct Password");
-          }
+            if(foundUser){
+                bcrypt.compare(req.body.password,foundUser.password,function(err,result){
+                  if(result){
+                      res.render("secrets");
+                  }else{
+                    res.send("Enter Correct Password");
+                  }
+                });
+            }else{
+              res.send("No User Found");
+            }
+
         }
     });
 });
